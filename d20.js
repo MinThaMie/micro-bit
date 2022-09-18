2c87a575e618b92423b17723b31ffff9e449f6c6
Anne-Greeth Schot-van Herwijnen <anne-greeth.vanherwijnen@nedap.com>
2022-09-18 09:47:22 +0200

    Update d20.js

--- d20.js
+++ d20.js
@@ -1,7 +1,5 @@
-let numb = 0
 input.onGesture(Gesture.Shake, function () {
-    // numb = randint(1,20)
-    numb = 20
+    const numb = randint(1,20)
     basic.showNumber(numb)
     if (numb == 20) {
         music.playSoundEffect(music.builtinSoundEffect(soundExpression.happy), SoundExpressionPlayMode.UntilDone)
