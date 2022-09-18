4daff99ec3544fc4ca73405ec4f79da33c6b3ab8
Anne-Greeth Schot-van Herwijnen <anne-greeth.vanherwijnen@nedap.com>
2022-09-18 10:37:21 +0200

    Add sad sound to natural one

--- d20.js
+++ d20.js
@@ -3,5 +3,7 @@ input.onGesture(Gesture.Shake, function () {
     basic.showNumber(numb)
     if (numb == 20) {
         music.playSoundEffect(music.builtinSoundEffect(soundExpression.happy), SoundExpressionPlayMode.UntilDone)
+    } else if (numb == 1) {
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.UntilDone)
     }
 })
